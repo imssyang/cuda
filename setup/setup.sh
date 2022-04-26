1d468d06f1397c2bfd7349f643ca5333f23bfe9a
imssyang <imssyang@gmail.com>
2022-04-26 15:59:42 +0800

    add tensorrt for python

--- setup/setup.sh
+++ setup/setup.sh
@@ -86,6 +86,7 @@ init() {
   $PIP install $HOME/setup/tensorrt/onnx_graphsurgeon-0.3.12-py2.py3-none-any.whl
   $PIP install $HOME/setup/tensorrt/tensorrt-8.2.4.2-cp38-none-linux_x86_64.whl
   $PIP install $HOME/setup/tensorrt/uff-0.6.9-py2.py3-none-any.whl
+  $PIP install 'pycuda<2021.1'
 }
 
 deinit() {
@@ -98,6 +99,7 @@ deinit() {
   $PIP uninstall $HOME/setup/tensorrt/onnx_graphsurgeon-0.3.12-py2.py3-none-any.whl
   $PIP uninstall $HOME/setup/tensorrt/tensorrt-8.2.4.2-cp38-none-linux_x86_64.whl
   $PIP uninstall $HOME/setup/tensorrt/uff-0.6.9-py2.py3-none-any.whl
+  $PIP uninstall 'pycuda<2021.1'
 }
 
 archive() {
