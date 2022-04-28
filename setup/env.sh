f10e3f7be2a21e56eac31c38771bbbb243038ee0
imssyang <imssyang@gmail.com>
2022-04-28 21:10:08 +0800

    disable pycuda

--- setup/env.sh
+++ setup/env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 export CUDA_DISABLE_ENV=yes
-export CUDA_ROOT=/usr/local/cuda/bin
+export CUDA_ROOT=/opt/cuda
+export CPATH=$CPATH:/opt/cuda/include
+export LIBRARY_PATH=$LIBRARY_PATH:/opt/cuda/lib64
 eval "optbin -s /opt/cuda/bin"
 eval "optlib -s /opt/cuda/lib64"
 
