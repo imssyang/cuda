62d0ee04ab437a1460f047190c0679d73a822bbc
imssyang <imssyang@gmail.com>
2024-04-09 17:52:18 +0800

    update env.sh

--- setup/env.sh
+++ setup/env.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export CUDA_DISABLE_ENV=yes
 export CUDA_ROOT=/opt/cuda
 export CPATH=$CPATH:/opt/cuda/include
 export LIBRARY_PATH=$LIBRARY_PATH:/opt/cuda/lib64
-eval "optbin -s /opt/cuda/bin"
-eval "optlib -s /opt/cuda/lib64"
-
